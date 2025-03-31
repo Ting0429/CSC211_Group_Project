28cbbba1a7f74d6873c85ab6a99c3824de099445
katyyyyyt <ekaterina.a.taranenko@gmail.com>
2025-03-30 23:36:43 -0400

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,41 @@
 #include <iostream>
-using namespace std;
+#include <fstream>
+#include <string>
+#include <iomanip>// Libraries that we are using in this project
 
-double findAverageHomeworkScore(double hw1, double hw2, double hw3, double hw4)
+#include "Student.h"// Our Class
+
+using namespace std; 
+
+int main()
 {
-    return((hw1 + hw2 + hw3 + hw4)/4.0);
-}
+   
+    cout << "Hello, World!" << endl; //testing line
 
-double findAverageLabScore(double lab1, double lab2, double lab3, double lab4, double lab5, double lab6, double lab7, double lab8)
-{
-    return((lab1 + lab2 + lab3 + lab4 + lab5 + lab6 + lab7 + lab8)/8.0);
-}
+    ofstream outFile("gradesOut1.txt"); // Kateryma's part
+    
+    //I commented it for the moment, because we don't have all functions
+    /*
+    if (outFile.is_open())
+    {
+        outFile << "Student Name: " << setw(10) << studentFirstName << setw(5) << studentLastName << endl;
+        outFile << "Attendance: " << setw(10) << studentAttendance << endl;
+        outFile << "Group Work: " << setw(10) << groupWork << endl;
+        outFile << "Average of the Quizzes: " << setw(10) << avgQuizScore << endl;
+        outFile << "Average of Labs: " << setw(10) << avgLabScore << endl;
+        outFile << "Average of Homework: " << setw(10) << avgHomeworkScore << endl;
+        outFile << "The Final Exam: " << setw(10) << finalExam << endl;
+        outFile << "The Course Average: " << setw(10) << courseAverage << endl;
+
+    }
+    else //in case of the error
+    {
+        cout << "We have trouble with creating your file. Please, try again later! ";
+        return 1;
+    }
+    */
+    outFile.close(); //closing the file
 
 
-int main() {
-    cout << "Hello, World!" << endl;
     return 0;
 }
