019ab5f5555bf65cd04a20ad2dc378dd10cdf4a4
Ting Wei Lin <tingwei.lin@cix.csi.cuny.edu>
2025-04-19 22:14:20 -0400

    Update Student.cpp

--- Student.cpp
+++ Student.cpp
@@ -6,8 +6,22 @@ using namespace std;
 // Ting Wei
 Student :: Student (string file, int stuNum){//Enter the file line of data the student starts at to initiate.
 //for loop to loop thru unused lines
-//cin while not last line
+//input for that student data
+ifstream infile;
+infile.open(file);
+int x;
 
+for (int i = 0; i < (stuNum*21); i++) {
+    infile.ignore();
+}
+
+infile >> studentFirstName >> studentLastName >> classPart
+>> lab1 >> lab2 >> lab3 >> lab4 >> lab5 >> lab6 >> lab7 >> lab8
+>> hw1 >> hw2 >> hw3 >> hw4
+>> quiz1 >> quiz2 >> quiz3 >> quiz4
+>> groupWork >> midterm >> final;
+
+}
 
 
 //Function "Find Average Homework Score"
