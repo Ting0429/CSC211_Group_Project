ea89d1bd672a8350db0ee1ca9af2372334fdfbdb
AbigailVincent <abigail.vincent@cix.csi.cuny.edu>
2025-04-20 01:26:54 -0400

    Update Student.h

--- Student.h
+++ Student.h
@@ -1,7 +1,8 @@
-#include <iostream>
-#include <fstream>
+#ifndef STUDENT_H
+#define STUDENT_H
+
 #include <string>
-#include <iomanip>
+#include <fstream>
 using namespace std;
 
 class Student {
@@ -18,66 +19,13 @@ public:
 
     static ifstream inFile;
 
-    Student() {
-        if (!inFile.is_open()) {
-            inFile.open("gradesIn.txt");
-        }
+    Student();
 
-        if (!inFile) {
-            cout << "Error opening file.\n";
-            return;
-        }
-
-        inFile >> firstName >> lastName >> attendance;
-
-        for (int i = 0; i < 8; i++) inFile >> labs[i];
-        for (int i = 0; i < 4; i++) inFile >> homework[i];
-        for (int i = 0; i < 4; i++) inFile >> quizzes[i];
-
-        inFile >> groupWork >> midterm >> finalExam;
-    }
-
-    double findAverageHomeworkScore() {
-        double sum = 0;
-        for (int i = 0; i < 4; i++) sum += homework[i];
-        return sum / 4.0;
-    }
-
-    double findAverageLabScore() {
-        double sum = 0;
-        for (int i = 0; i < 8; i++) sum += labs[i];
-        return sum / 8.0;
-    }
-
-    double findAverageQuizScore() {
-        double minQuiz = quizzes[0];
-        double sum = quizzes[0];
-        for (int i = 1; i < 4; i++) {
-            sum += quizzes[i];
-            if (quizzes[i] < minQuiz) minQuiz = quizzes[i];
-        }
-        return (sum - minQuiz) / 3.0;
-    }
-
-    double calculateWeightedAverage() {
-        return (attendance * 0.10) +
-               (findAverageLabScore() * 0.15) +
-               (findAverageHomeworkScore() * 0.15) +
-               (findAverageQuizScore() * 0.15) +
-               (groupWork * 0.05) +
-               (midterm * 0.20) +
-               (finalExam * 0.20);
-    }
-
-    string calculateLetterGrade(double weightedAverage) {
-        if (weightedAverage >= 90) return "A";
-        else if (weightedAverage >= 80) return "B";
-        else if (weightedAverage >= 75) return "C+";
-        else if (weightedAverage >= 70) return "C";
-        else if (weightedAverage >= 60) return "D";
-        else return "F";
-    }
+    double findAverageHomeworkScore();
+    double findAverageLabScore();
+    double findAverageQuizScore();
+    double calculateWeightedAverage();
+    string calculateLetterGrade(double weightedAverage);
 };
 
-
-
+#endif
