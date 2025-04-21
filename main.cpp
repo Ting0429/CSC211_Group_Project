1403b9a3067dc1ad6d5453197d4fbf122d1464cc
AbigailVincent <abigail.vincent@cix.csi.cuny.edu>
2025-04-20 22:01:41 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,16 +6,17 @@
 using namespace std;
 
 int main() {
+    //array to hold the Student objects
     Student students[5];
-
+    //creating and opening two output files
     ofstream outFile1("gradesOut1.txt");
     ofstream outFile2("gradesOut2.txt");
-
+    //making sure it can open
     if (!outFile1 || !outFile2) {
         cout << "Error opening output files.\n";
         return 1;
     }
-
+    //setting output format
     outFile1 << fixed << setprecision(2);
     outFile2 << fixed << setprecision(2);
 
@@ -25,6 +26,7 @@ int main() {
     for (int i = 0; i < totalStudents; i++) {
         //Student &s is updated with the for loop and creating a reference to students[i]
         Student& s = students[i];
+        //calculating course weighted avg and letter grade
         double courseAvg = s.calculateWeightedAverage();
         string letter = s.calculateLetterGrade(courseAvg);
 
@@ -44,13 +46,13 @@ int main() {
                  << "Course Average: " << courseAvg << endl
                  << "Letter Grade: " << letter << endl
                  << "--------------------------------------" << endl;
-
+        //adding to class overall average later
         totalCourseAverage += courseAvg;
     }
 
     outFile2 << "Total number of students: " << totalStudents << endl;
     outFile2 << "Class average: " << (totalCourseAverage / totalStudents) << endl;
-
+    //closing output files
     outFile1.close();
     outFile2.close();
 
