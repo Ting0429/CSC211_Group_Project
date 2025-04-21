da1acd3eaaeae21cefe891687d893ec2f5389c5f
Ting Wei Lin <tingwei.lin@cix.csi.cuny.edu>
2025-04-20 23:00:14 -0400

    Update Student.cpp

--- Student.cpp
+++ Student.cpp
@@ -14,13 +14,12 @@ using namespace std;
 Student :: Student (string file, int stuNum){//Enter the file line of data the student starts at to initiate.
     ifstream infile;
     infile.open(file);
-        if (stuNum <= 5){
+    if (stuNum <= 5&& stuNum > 0) {//if the student number is valid
         string temp;
-            
         for (int i = 0; i < (stuNum-1)*9; i++) {//skips thru unnecessary lines
             getline (infile, temp);
         }
-            
+        
         infile >> studentFirstName >> studentLastName >> classPart
         >> lab[0] >> lab[1] >> lab[2] >> lab[3] >> lab[4] >> lab[5] >> lab[6] >> lab[7]
         >> hw[0] >> hw[1] >> hw[2] >> hw[3]
@@ -28,7 +27,6 @@ Student :: Student (string file, int stuNum){//Enter the file line of data the s
         >> groupWork >> midterm >> finalGrade;//stores the data into the variables
     }
     else {
-        cout << "Invalid student number." << endl;
+        cout << "Invalid student number." << endl; //if there is no such student number
     }
 }
-
