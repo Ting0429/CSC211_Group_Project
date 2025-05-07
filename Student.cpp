f1fc289c5b63240f05e4de151d4cede0dff91c3b
Ting Wei Lin <tingwei.lin@cix.csi.cuny.edu>
2025-05-07 11:46:41 -0400

    Update Student.cpp

--- Student.cpp
+++ Student.cpp
@@ -11,14 +11,15 @@
 #include <fstream>
 using namespace std;
 // Ting Wei
-Student :: Student (string file, int stuNum){//Enter the file line of data the student starts at to initiate.
+Student :: Student (string file, int stuNum){
+    //Enter the file line of data the student starts at to initiate.
     ifstream infile;
     infile.open(file);
     if (stuNum <= 5&& stuNum > 0) {//if the student number is valid
         string temp;
-        for (int i = 0; i < (stuNum-1)*9; i++) {//skips thru unnecessary lines
+        for (int i = 0; i < (stuNum-1)*9; i++) {
             getline (infile, temp);
-        }
+        }//skips thru unnecessary lines
         
         infile >> studentFirstName >> studentLastName >> classPart
         >> lab[0] >> lab[1] >> lab[2] >> lab[3] >> lab[4] >> lab[5] >> lab[6] >> lab[7]
